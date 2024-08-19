7d2779f17844c05a408dc2fbc7bb05e377f4ea62
dmighty007 <dibyendumaity1999@bose.res.in>
2024-08-20 00:33:16 +0530

    minor changes

--- scripts/member_update.py
+++ scripts/member_update.py
@@ -1,6 +1,6 @@
 import numpy as np
 import json
-def add_mem(name, desig, sub, img, fb, tw, insta, linkedin, style = 1, interest = ""):
+def add_mem(name, desig, sub, img, mail, tw, fb, linkedin, style = 1, interest = ""):
     """
         This function generates a formatted HTML snippet for a team member's card.
 
@@ -8,7 +8,7 @@ def add_mem(name, desig, sub, img, fb, tw, insta, linkedin, style = 1, interest 
       name (str): The name of the team member.
       desig (str): The designation of the team member.
       img (str): The URL of the team member's image.
-      fb (str): The URL of the team member's Facebook profile.
+      mail (str): The URL of the team member's Facebook profile.
       tw (str): The URL of the team member's Twitter profile.
       insta (str): The URL of the team member's Instagram profile.
       linkedin (str): The URL of the team member's LinkedIn profile.
@@ -23,12 +23,12 @@ def add_mem(name, desig, sub, img, fb, tw, insta, linkedin, style = 1, interest 
                                 <img src="{img}" class="img-fluid w-100 rounded-top" alt="Image">
                             </div>
                             <div class="guide-icon rounded-pill p-2">
-                                <a class="btn btn-square btn-primary rounded-circle mx-1" href="{fb}"><i
-                                        class="fab fa-facebook-f"></i></a>
+                                <a class="btn btn-square btn-primary rounded-circle mx-1" href="mailto:{mail}"><i
+                                        class="fa fa-envelope"></i></a>
                                 <a class="btn btn-square btn-primary rounded-circle mx-1" href="{tw}"><i
                                         class="fab fa-twitter"></i></a>
-                                <a class="btn btn-square btn-primary rounded-circle mx-1" href="{insta}"><i
-                                        class="fab fa-instagram"></i></a>
+                                <a class="btn btn-square btn-primary rounded-circle mx-1" href="{fb}"><i
+                                        class="fab fa-facebook"></i></a>
                                 <a class="btn btn-square btn-primary rounded-circle mx-1" href="{linkedin}"><i
                                         class="fab fa-linkedin-in"></i></a>
                             </div>
@@ -55,7 +55,7 @@ def add_mem(name, desig, sub, img, fb, tw, insta, linkedin, style = 1, interest 
                                 <a href="#" class="h4">{name}</a>
                                 <p class="my-3">{interest}</p>
                                 <div class="guide-icon rounded-pill p-2" style="display: flex; margin: auto; text-align: center; justify-content: center;">
-                                <a class="btn btn-square btn-primary rounded-circle mx-1" href="{fb}"><i
+                                <a class="btn btn-square btn-primary rounded-circle mx-1" href="mailto:{mail}"><i
                                         class="fa fa-envelope"></i></a>
                                 <a class="btn btn-square btn-primary rounded-circle mx-1" href="{fb}"><i
                                         class="fab fa-facebook-f"></i></a>
@@ -83,9 +83,9 @@ def add_alumni(name, desig, role,  img, mail ):
                         </div>
                         <div class="guide-title text-center rounded-bottom p-4">
                             <div class="guide-title-inner">
-                                <p class="mt-2">{role}</p>
+                                <p class="mb-0 mt-2"><strong style="color:black;margin:auto; padding:auto;">Role: </strong>{role}</p>
                                 <h4 class="mt-0">{name}</h4>
-                                <p class="mb-0">{desig}</p>
+                                <p class="mb-0"><strong style="color:red;">Current: </strong>{desig}</p>
                             </div>
                         </div>
                     </div>
@@ -137,24 +137,54 @@ for item in infos:
         head += section_head
         for mem in infos[item]:
             head += add_mem(name = mem['name'], desig = mem['desig'], sub = mem['sub'],
-                            img = mem['img'], fb = '#', tw = '#', insta = '#',
-                            linkedin = '#', style = 2, interest=mem['interest'])
+                            img = mem['img'], mail = mem['mail'], tw = mem['tw'], fb = mem['fb'],
+                            linkedin = mem['linkedin'], style = 2, interest=mem['interest'])
         head += section_tail
     elif item == 'postdoc':
         section_head, section_tail = return_head_tail('Postdoctoral Fellows')
         head += section_head
         for mem in infos[item]:
             head += add_mem(name = mem['name'], desig = mem['desig'], sub = "",
-                            img = mem['img'], fb = '#', tw = '#', insta = '#',
-                            linkedin = '#', style = 1, interest="")
+                            img = mem['img'], mail = mem['mail'], tw = mem['tw'], fb = mem['fb'],
+                            linkedin = mem['linkedin'], style = 1, interest=mem['interest'])
         head += section_tail
     elif item == 'project':
         section_head, section_tail = return_head_tail('Project Students')
         head += section_head
-        for mem in infos[item]:
-            head += add_mem(name = mem['name'], desig = "TRC Project" , sub = "",
-                            img = mem['img'], fb = '#', tw = '#', insta = '#',
-                            linkedin = '#', style = 1, interest = "Drug design")
+        if len(infos[item]):
+            for mem in infos[item]:
+                head += add_mem(name = mem['name'], desig = mem['desig'] , sub = "",
+                                img = mem['img'], mail = mem['mail'], tw = mem['tw'], fb = mem['fb'],
+                                linkedin = mem['linkedin'], style = 1, interest = mem['interest'])
+        else:
+            head += """
+                            <div class="col-md-6 col-lg-3">
+                    <div class="guide-item">
+                        <div class="guide-img">
+                            <div class="guide-img-efects">
+                                <img src="./img/avatar_scholar_128.png" class="img-fluid w-100 rounded-top" alt="Image">
+                            </div>
+                        </div>
+                        <div class="guide-title text-center rounded-bottom p-4">
+                            <div class="guide-title-inner" style="margin-top:10px;">
+                                <p class="mb-0">Project</p>
+                                <h4 class="mt-0">Fname Lname</h4>
+                                <p class="mb-0"></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6 col-lg-6" style="margin: auto;">
+                    <div class="guide-item">
+                        <h4 class="mt-0 mb-5">Currently, we have no project students, but we'd love to collaborate with
+                            you! If you're interested in working with us,
+                            please reach out. For more details, click the link below.</h4>
+                        <a class="btn-hover-bg btn btn-primary rounded-pill text-white py-3 px-5" href="positions.html">
+                            Oportunities</a>
+                    </div>
+                </div>
+            """
+            
         head += section_tail
     full_html += head
 full_html += ''.join(tail)
